d5b177356e5b7c4f22d0ac34b57d24e794ce3ba8
SomeoneProject <3277673229@qq.com>
2020-04-20 14:21:16 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Bing_Wallpaper_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "1920×1080",
+            "1366×768",
+            "1280×768",
+            "1024×768",
+            "800×600",
+            "800×480",
+            "768×1280",
+            "720×1280",
+            "640×480",
+            "480×800",
+            "400×240",
+            "320×240",
+            "240×320"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "请选择壁纸图片的分辨率";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 237);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(276, 276);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(276, 276);
+            this.Name = "Form1";
+            this.Text = "Bing Wallpaper 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Bing_Wallpaper_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int iActulaWidth;
+        private int iActulaHeight;
+        private string key;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            iActulaWidth = Screen.PrimaryScreen.Bounds.Width;
+            iActulaHeight = Screen.PrimaryScreen.Bounds.Height;
+            label1.Text = $"请选择壁纸图片的分辨率({iActulaWidth}×{iActulaHeight})";
+            foreach (string item in listBox1.Items)
+            {
+                if (item == $"{iActulaWidth}×{iActulaHeight}")
+                {
+                    listBox1.SelectedIndex = listBox1.Items.IndexOf(item);
+                    timer1.Enabled = true;
+                    break;
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                iActulaWidth = int.Parse(listBox1.SelectedItem.ToString().Substring(0, listBox1.SelectedItem.ToString().IndexOf("×")));
+                iActulaHeight = int.Parse(listBox1.SelectedItem.ToString().Substring(listBox1.SelectedItem.ToString().IndexOf("×") + 1));
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message, "Bing Wallpaper 2");
+                return;
+            }
+            listBox1.Enabled = false;
+            button1.Enabled = false;
+            label1.Text = "等待中";
+            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)}\\Bing Wallpaper";
+            if (path.Substring(path.Length - 1) != "\\")
+            {
+                path += "\\";
+            }
+            if (DateTime.Today.Year.ToString().Length != 4)
+            {
+                path += "0000".Substring(4 - DateTime.Today.Year.ToString().Length);
+            }
+            path += DateTime.Today.Year.ToString();
+            if (DateTime.Today.Month.ToString().Length != 2)
+            {
+                path += "00".Substring(2 - DateTime.Today.Month.ToString().Length);
+            }
+            path += DateTime.Today.Month.ToString();
+            if (DateTime.Today.Day.ToString().Length != 2)
+            {
+                path += "00".Substring(2 - DateTime.Today.Day.ToString().Length);
+            }
+            path += DateTime.Today.Day.ToString();
+            path += ".jpg";
+            if (DownLoadFile($"https://api.berryapi.net/?service=App.Bing.Images&w={iActulaWidth}&h={iActulaHeight}", path, (int Maximum, int Value) =>
+        {
+            key = "Byte";
+            if (Value >= 1024)
+            {
+                Value /= 1024;
+                key = "KiloByte";
+            }
+            if (Value > 1)
+            {
+                key += "s";
+            }
+            label1.Text = $"已下载 {Value} {key}";
+        }))
+            {
+                SystemParametersInfo(20, 0, path, 0x2);
+                Application.Exit();
+            }
+            else
+            {
+                listBox1.Enabled = true;
+                button1.Enabled = true;
+                label1.Text = "出现错误，请稍后再试";
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Button1_Click(null, null);
+        }
+
+        /*---------------------------*\
+        |* 没错，下面都不是我自己写的*|
+        \*---------------------------*/
+
+        /// <summary>  
+        /// 下载带进度条代码（普通进度条）  
+        /// </summary>  
+        /// <param name="URL">网址</param>  
+        /// <param name="Filename">下载后文件名为</param>  
+        /// <param name="Prog">报告进度的处理(第一个参数：总大小，第二个参数：当前进度)</param>  
+        /// <returns>True/False是否下载成功</returns>  
+        public static bool DownLoadFile(string URL, string Filename, Action<int, int> updateProgress = null)
+        {
+            Stream st = null;
+            Stream so = null;
+            HttpWebRequest Myrq = null;
+            HttpWebResponse myrp = null;
+            bool flag = false;
+            try
+            {
+                Myrq = (HttpWebRequest)WebRequest.Create(URL); //从URL地址得到一个WEB请求     
+                myrp = (HttpWebResponse)Myrq.GetResponse(); //从WEB请求得到WEB响应     
+                long totalBytes = myrp.ContentLength; //从WEB响应得到总字节数
+                                                      //更新进度
+                updateProgress?.Invoke((int)totalBytes, 0);//从总字节数得到进度条的最大值  
+                st = myrp.GetResponseStream(); //从WEB请求创建流（读）     
+                so = new FileStream(Filename, FileMode.Create); //创建文件流（写）     
+                long totalDownloadedByte = 0; //下载文件大小     
+                byte[] by = new byte[1024];
+                int osize = st.Read(by, 0, by.Length); //读流     
+                while (osize > 0)
+                {
+                    totalDownloadedByte = osize + totalDownloadedByte; //更新文件大小     
+                    Application.DoEvents();
+                    so.Write(by, 0, osize); //写流     
+                                            //更新进度
+                    updateProgress?.Invoke((int)totalBytes, (int)totalDownloadedByte);//更新进度条 
+                    osize = st.Read(by, 0, by.Length); //读流     
+                }
+                //更新进度
+                updateProgress?.Invoke((int)totalBytes, (int)totalBytes);
+                flag = true;
+            }
+            catch (Exception error)
+            {
+                flag = false;
+                MessageBox.Show(error.Message, "Bing Wallpaper 2");
+            }
+            finally
+            {
+                if (Myrq != null)
+                {
+                    Myrq.Abort();//销毁关闭连接
+                }
+                if (myrp != null)
+                {
+                    myrp.Close();//销毁关闭响应
+                }
+                if (so != null)
+                {
+                    so.Close(); //关闭流 
+                }
+                if (st != null)
+                {
+                    st.Close(); //关闭流  
+                }
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 系统参数信息
+        /// </summary>
+        /// <param name="uAction">20</param>
+        /// <param name="uParam">0</param>
+        /// <param name="lpvParam">file</param>
+        /// <param name="fuWinIni">0x2</param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+}
